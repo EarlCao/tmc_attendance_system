3d8c9e27fbe73b3989bfb6ef19a7ec11978d6823
Earl Francis Cao <earlfranciscao.yt.com>
2026-05-26 19:42:10 +0800

    prisma migrate database

--- server-tmc-choir-system/src/lib/prisma.js
+++ server-tmc-choir-system/src/lib/prisma.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { PrismaMariaDb } from '@prisma/maria-db';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from '../../generated/prisma/client.js';
 
 const adapter = new PrismaMariaDb({
---
+++ server-tmc-choir-system/src/server.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import { prisma } from "./lib/prisma.js";
+import express from "express";
+import { createServer } from "http";
+import cors from "cors";
+import helmet from "helmet";
+
+const BACKEND_PORT = process.env.BACKEND_PORT;
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+process.on('SIGTERM', async () => await prisma.$disconnect());
+
+httpServer.listen(BACKEND_PORT, () => {
+  console.log(`Server is running on port ${BACKEND_PORT}`);
+});
